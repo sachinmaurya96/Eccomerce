09c0b3d9b0eb018d88c812ee166ba7a5a41cc9b6
sachinmaurya96 <125910801+sachinmaurya96@users.noreply.github.com>
2024-01-25 12:29:52 +0530

    typograph and errorpage compleate

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Home = () => {
   return (
-    <div>
-      homepage
+    <Wrapper>
+      <div className='container'>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Quo itaque sequi rerum vero eum minima corrupti fuga aspernatur eligendi ad?</p>
+      <a href="">Lorem, ipsum dolor.</a>
     </div>
+    </Wrapper>
   )
 }
-
+const Wrapper = styled.section`
+  
+  
+`
 export default Home
