b98d8049176ea7225c6ec5e4072b402eed4fc829
sachinmaurya96 <125910801+sachinmaurya96@users.noreply.github.com>
2024-02-01 21:11:42 +0530

    checkout page compleate

--- src/component/Protected.jsx
+++ src/component/Protected.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Protected = ({children,authentication=true}) => {
-    const authStatus = false
+    const authStatus = true
     const navigate = useNavigate()
 
     useEffect(()=>{
